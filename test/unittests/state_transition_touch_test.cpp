ec79d3466056a75c79a78a5e5c700813a1056d37
Paweł Bylica <pawel@ethereum.org>
2024-01-01 15:34:07 +0100

    add test showing bug

--- test/unittests/state_transition_touch_test.cpp
+++ test/unittests/state_transition_touch_test.cpp
@@ -214,3 +214,20 @@ TEST_F(state_transition, touch_revert_selfdestruct_to_nonexistient_tw)
     expect.post[DESTRUCTOR].exists = true;
     expect.post[BENEFICIARY].exists = false;
 }
+
+TEST_F(state_transition, call_loop)
+{
+    rev = EVMC_HOMESTEAD;
+    static constexpr auto EMPTY = 0x04_address;
+
+    tx.to = To;
+    tx.gas_limit = 150000;
+    pre.insert(*tx.to,
+        {.balance = 0xfffffffffffff,
+            .code =
+                "5b61c3506080511015602c576000600061c35060006001600461061cf16000556001608051016080526000565b608051600155"_hex});
+
+    expect.status = EVMC_OUT_OF_GAS;
+    expect.post[*tx.to].exists = true;
+    expect.post[EMPTY].exists = false;
+}
