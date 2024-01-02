c3c745c61236c6e05d8fa298725e6e821debb8ba
Paweł Bylica <pawel@ethereum.org>
2024-01-02 21:57:46 +0100

    precompiles: Expose internal API
    Expose all `xxx_analyze()` and `xxx_execute()` functions from
    the `evmone::state` library. They are usable for tests and benchmarks.
    
    Make `EVMONE_PRECOMPILES_SILKPRE` compile definition available to
    the `evmone::state`s users.

--- test/state/precompiles.hpp
+++ test/state/precompiles.hpp
@@ -43,4 +43,31 @@ bool is_precompile(evmc_revision rev, const evmc::address& addr) noexcept;
 
 /// Executes the message to a precompiled contract (msg.code_address must be a precompile).
 evmc::Result call_precompile(evmc_revision rev, const evmc_message& msg) noexcept;
+
+
+struct PrecompileAnalysis
+{
+    int64_t gas_cost;
+    size_t max_output_size;
+};
+
+PrecompileAnalysis ecrecover_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis sha256_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis ripemd160_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis identity_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis expmod_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis ecadd_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis ecmul_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis ecpairing_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis blake2bf_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+PrecompileAnalysis point_evaluation_analyze(evmc::bytes_view input, evmc_revision rev) noexcept;
+
+ExecutionResult ecrecover_execute(
+    const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
+ExecutionResult identity_execute(
+    const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
+ExecutionResult ecadd_execute(
+    const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
+ExecutionResult ecmul_execute(
+    const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
 }  // namespace evmone::state
